398059da7d28774e1b34aee1de1b94da5a876fc8
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-26 20:50:57 +0200

    added event listeners for the navigation

--- site.js
+++ site.js
@@ -1,7 +1,7 @@
-const btnNav = document.querySelector(".btn-mobile-nav");
+// const btnNav = document.querySelector(".btn-mobile-nav");
 
-const header = document.querySelector(".header");
+// const header = document.querySelector(".header");
 
-btnNav.addEventListener("click", function () {
-  header.classList.toggle("nav-open");
-});
+// btnNav.addEventListener("click", function () {
+//   header.classList.toggle("nav-open");
+// });
