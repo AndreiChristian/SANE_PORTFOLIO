e99bb303847b3e76acd3f33122a12c7cd99a024b
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-01 15:31:03 +0200

    lets gooo

--- site.js
+++
@@ -1,7 +0,0 @@
-// const btnNav = document.querySelector(".btn-mobile-nav");
-
-// const header = document.querySelector(".header");
-
-// btnNav.addEventListener("click", function () {
-//   header.classList.toggle("nav-open");
-// });
